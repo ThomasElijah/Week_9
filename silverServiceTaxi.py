65dfc94e1eb2a2ad3f67da9efb1f08d807f70a0c
ThomasElijah <elijah.thomas@my.jcu.edu.au>
2016-09-20 18:38:54 +1000

    test

---
+++ silverServiceTaxi.py
@@ -0,0 +1,18 @@
+from Taxi import Taxi
+
+class SilverServiceTaxi(Taxi):
+    flagFall = 4.50
+
+    def __init__(self, name , fuel, fanciness):
+        super().__init__(name,fuel)
+        self.fanciness = fanciness
+
+    def get_fare(self):
+        return(self.current_fare_distance * (Taxi.price_per_km * self.fanciness) + SilverServiceTaxi.flagFall)
+
+
+def test():
+    ssvTest = SilverServiceTaxi("ssvTest", 200, 2)
+    ssvTest.drive(10)
+    print("The cost of this trip is ${:.2f}".format(ssvTest.get_fare()))
+test()
